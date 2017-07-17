1a726f8e596f7c9f26e0dd93c9975d798d34b0d7
Katharina-Maria Heer <kathy11997@gmx.de>
2017-07-17 15:40:44 +0200

    User Routen

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,9 @@
-user.js
+var express = require('express');
+var router = express.Router();
+
+var usercontroller = require('../controller/usercontroller');
+
+router.put('/login', usercontroller.login);
+router.put('/passwordRecovery', usercontroller.changepassword);
+
+module.exports = router;
