134cb9fb640dbc27f85506bf8affafaf631081c8
Katharina-Maria Heer <kathy11997@gmx.de>
2017-07-17 16:04:32 +0200

    usercontroller

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,1 +1,10 @@
-usercontroller.js
+var jwt = require('jsonwebtoken');
+var user = './db/user.json';
+
+exports.login = function (req, res) {
+   
+};
+
+exports.changePassword = function (req, res) {
+
+}
