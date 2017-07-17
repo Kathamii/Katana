60fa3c5f23bfe6ddd2d5f67f31b898c31dceb37f
Katharina-Maria Heer <kathy11997@gmx.de>
2017-07-17 16:45:17 +0200

    login & change password

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,11 +1,59 @@
 var jwt = require('jsonwebtoken');
 var userfile = './db/user.json';
+let user = require('../db/user.json');
 
 exports.login = function (req, res) {
    
+    //Falscher Username oder falsches Passwort?
+    if (req.body.username != userfile.username || req.body.password != userfile.password)
+    {
+        res.status(401).json
+         (
+            {
+            message: 'Invalid Username or Password!'
+            }
+         );
+       return;
+    }
+
+
+    //Wenn alles passt, Token generieren und schicken
+    var token = jwt.sign(user, 'KatanasSignature', {
+        expires: 86400
+    });
+
+    res.status(200).json({ 'token': token });
+
+
 };
 
 exports.changePassword = function (req, res) {
+
+
+    //Eingeloggt?
+    if (!res.locals.authenticated) {
+        res.status(403).send();
+        return;
+    }
+
+    //Richtiges Passwort?
+    if (req.body.password != user.password) {
+        res.status(401).json({
+            message: 'Invalid Password!'
+        });
+        return;
+    }
+
+    //Passwort ändern
+    user.password = req.body.newPassword;
+
+
+    //neuen Token ertsellen und senden
+    var token = jwt.sign(user, 'KatanasSignature', {
+        expires: 86400
+    });
+
+    res.status(200).json({ 'token': token });
 
 };
 
