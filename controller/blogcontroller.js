43a2deb246fdb35221b9ac708d0580e1de1e8560
Katharina-Maria Heer <kathy11997@gmx.de>
2017-07-17 16:11:10 +0200

    blogcontroller

--- controller/blogcontroller.js
+++ controller/blogcontroller.js
@@ -1,1 +1,23 @@
-blogcontroller.js
+var blog = require('../data/blog.json');
+var ObjectID = require('mongodb').ObjectID;
+
+
+exports.getblogentrys = function (req, res) {
+
+};
+
+exports.postentry = function (req, res) {
+
+};
+
+exports.getblogentrybyid = function (req, res) {
+
+};
+
+exports.deleteblogentrybyid = function (req, res) {
+
+};
+
+exports.updateblogentrybyid = function (req, res) {
+
+};
