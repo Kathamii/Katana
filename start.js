681d669fade23708e42ce6ae626b8963a1d62635
Kathamii <kathy11997@gmx.de>
2017-07-23 13:18:35 +0200

    update authentification

--- start.js
+++ start.js
@@ -4,11 +4,15 @@ var bodyparser = require('body-parser');
 
 var user = require('./routes/user.js');
 var blog = require('./routes/blog.js');
+var authentification = require('./controller/authentificationcontroller.js');
+
 
 var port = 3000;
 
 app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
+app.use('/api/V1', authentification);
 app.use('/api/V1', user);
 app.use('/api/V1/blog', blog);
 
